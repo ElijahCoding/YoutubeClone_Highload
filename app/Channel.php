fc12ce4951b220e22c7be9ae4d948b965f68568f
ElijahCoding <cuiyeqing960904@gmail.com>
2019-09-11 12:41:42 +0300

    using spatie-media-library to handle images

--- app/Channel.php
+++ app/Channel.php
@@ -3,9 +3,13 @@
 namespace App;
 
 use App\{Model, User};
+use Spatie\MediaLibrary\HasMedia\HasMedia;
+use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
 
-class Channel extends Model
+class Channel extends Model implements HasMedia
 {
+    use HasMediaTrait;
+
     public function user()
     {
         return $this->belongsTo(User::class);
